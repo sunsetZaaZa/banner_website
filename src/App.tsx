da8b0842934ea8af43a22a22bdb4903bc8e3dc73
Vincent <151476170+sunsetZaaZa@users.noreply.github.com>
2025-02-23 17:49:16 -0600

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+import WorkshopTools from './tablets/WorkshopTools'
+
+function App() {
+
+  return (
+    <>
+      <WorkshopTools/>
+    </>
+  )
+}
+
+export default App
